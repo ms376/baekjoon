a5fe57fe041a225b90ffa274cac25348283e8a84
ms <>
2023-09-17 23:41:31 +0900

    ~23.09.17

--- src/chap3_loop/Main3.java
+++ src/chap3_loop/Main3.java
@@ -3,15 +3,11 @@ package chap3_loop;
 import java.util.Scanner;
 
 public class Main3 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		long n = sc.nextLong();
-		for (int i = 1; i < n; i++) {
-			long c= (i+1);
-		}
-		
+		long a = ((n*(n+1)/2));
+		System.out.println(a);
 		sc.close();
 	}
-
 }
