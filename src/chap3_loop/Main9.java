1f7e7c8ec04676a23f9dda20c84ed863f5d20a3a
ms <>
2023-09-23 10:18:44 +0900

    23.09.23

---
+++ src/chap3_loop/Main9.java
@@ -0,0 +1,19 @@
+package chap3_loop;
+
+import java.util.Scanner;
+
+public class Main9 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < i; j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println("*");
+		}
+	}
+
+}
